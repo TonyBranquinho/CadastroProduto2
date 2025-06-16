fdb8415bd3260a9ff1382c21c5a6ac110d7aa2c7
Tony <antony.branquinho@gmail.com>
2025-06-16 20:43:57 -0300

    agora le e atualiza produtos

--- CadastroProduto2/Program.cs
+++ CadastroProduto2/Program.cs
@@ -17,6 +17,9 @@ namespace MyApp
             // Chama o metodo ESTATICO Carregar para preencher a lista
             List<Produto> listaDeProdutos = OperacaoBancoDados.Carregar();
 
+            // Instancia um objeto para verificaçao do id
+            ProdutoServico produtoServico = new ProdutoServico(listaDeProdutos);
+
             // Imprime a quantidade de objetos na lista
             Console.WriteLine("Itens carregados: " + listaDeProdutos.Count);
 
@@ -24,38 +27,136 @@ namespace MyApp
             Console.WriteLine();
             ProdutoImprimir.Imprimir(listaDeProdutos);
 
-            // Cadastra novos produtos
+
+            // Cadastra novos produtos - CRUD - CREATE
             Console.WriteLine();
             Console.Write("Digite SIM ou NAO para cadastrar novos produtos: ");
             string simOuNao = Console.ReadLine();
 
-            if (simOuNao == "sim")
-            {                
+            if (simOuNao.ToLower() == "sim")
+
+            {
                 Console.Write("Digite o ID do produto: ");
                 int id = int.Parse(Console.ReadLine());
+
+                // Verifica a disponibilidade do ID               
+                bool testaId = produtoServico.VerificaId(listaDeProdutos, id);
+
+                while (testaId == false)
+                {
+                    Console.Write("Esse id nao esta disponivel, digite outro:");
+                    id = int.Parse(Console.ReadLine());
+                    testaId = produtoServico.VerificaId(listaDeProdutos, id);
+                }
+
                 Console.Write("Digite o NOME do Produto: ");
                 string nome = Console.ReadLine();
                 Console.Write("Digite o PRECO do produto: ");
                 double preco = double.Parse(Console.ReadLine());
                 Console.Write("Digite a QUANTIDADE do produto: ");
                 int quantidade = int.Parse(Console.ReadLine());
-                               
-                // cria um objeto com todos os atributos da classe protudo
-                Produto produto = new Produto(id, nome, preco, quantidade);
 
-                // instancia objeto do tipo ProdutoServico
+                // cria um objeto com todos os atributos da classe protudo
+                Produto novoProduto = new Produto(id, nome, preco, quantidade);
+
+                // instancia objeto do tipo ProdutoServico passando a lista JSON
                 ProdutoServico servico = new ProdutoServico(listaDeProdutos);
 
-                // chama metodo para salvar o objeto na lista
-                servico.Cadastro(produto);
-                
-                // salvo os dados da lista em JSON no arquivo 
+                // chama metodo para salvar o novo objeto na lista
+                servico.Cadastro(novoProduto);
+
+                // coverte os dados da lista em JSON e os salva no arquivo JSON
                 OperacaoBancoDados.Salvar(listaDeProdutos);
 
+                // mostra o item que foi adicionado na lista
                 Console.WriteLine();
                 Console.WriteLine("Este item foi cadastrado:");
-                Console.WriteLine(string.Join(" - ", produto.ToString()));
-            }            
+                Console.WriteLine(string.Join(" - ", novoProduto.ToString()));
+            }
+
+
+            // Busca e imprime um produto especifico - CRUD - READ
+            Console.WriteLine();
+            Console.Write("Quer Imprimir algum item especifico? ");
+            simOuNao = Console.ReadLine();
+
+            if (simOuNao.ToLower() == "sim")
+            {
+                Console.Write("Digite o ID do produto a ser impresso: ");
+                int buscaId = int.Parse(Console.ReadLine());
+
+                // instancia uma variavel e chama o metodo de BuscaProduto
+                Produto produtoAImprimir = produtoServico.BuscaProduto(buscaId);
+
+                Console.WriteLine(produtoAImprimir.ToString());
+            }
+
+
+            // Atualiza um produto - CRUD - UPDATE
+            Console.WriteLine();
+            Console.Write("Quer alterar algum produto da lista? ");
+            simOuNao = Console.ReadLine();
+
+            if (simOuNao.ToLower() != "nao")
+            {
+                Console.Write("Digite o ID do produto a ser alterado: ");
+                int buscaId = int.Parse(Console.ReadLine());
+
+                // atribui a um novo objeto o retorno do metodo BuscaProduto
+                Produto produtoASerAlterado = produtoServico.BuscaProduto(buscaId);
+
+                // cadastra NOME
+                Console.Write("Digite o NOME do produto: ");
+                produtoASerAlterado.Nome = Console.ReadLine();
+                
+                // cadastra PREÇO
+                Console.Write("Digite o PRECO do produto: ");
+                if (!double.TryParse(Console.ReadLine(), out double preco))
+                {
+                    Console.WriteLine("Preço invalido, tente novamente.");
+                    return;
+                }
+                produtoASerAlterado.Preco = preco;
+                
+                // cadastra QUANTIDADE
+                Console.Write("Digite a QUANTIDADE: ");
+                if (!int.TryParse(Console.ReadLine(), out int quantidade))
+                {
+                    Console.WriteLine("Quantidade invalida, tente novamente.");
+                    return;
+                }
+                produtoASerAlterado.Quantidade = quantidade;
+
+                // salva a lista no arquivo JSON
+                OperacaoBancoDados.Salvar(listaDeProdutos);
+                Console.WriteLine("Esse item foi alterado:" + produtoASerAlterado);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Exclui um produto - CRUD - DELETE
+            Console.WriteLine();
+            Console.Write("Quer excluir algum produto da lista? ");
+            simOuNao = Console.ReadLine();
+
+            if (simOuNao == "sim")
+            {
+
+            }
         }
     }
 }
--- CadastroProduto2/Servicos/ProdutoServico.cs
+++ CadastroProduto2/Servicos/ProdutoServico.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace CadastroProduto2.Servicos
@@ -11,19 +12,82 @@ namespace CadastroProduto2.Servicos
     {
         public List<Produto> lista { get; private set; }
 
-        public ProdutoServico() 
+        // Construtor vazio
+        public ProdutoServico()
         {
         }
 
+        // Construtor recebendo a lista de Json como parametro
         public ProdutoServico(List<Produto> listaDeProdutos)
         {
             lista = listaDeProdutos;
         }
 
-        // CADASTRA PRODUTO
-        public void Cadastro(Produto produto)
+        // Metodo para Cadastrar produto
+        public void Cadastro(Produto novoProduto)
         {
-            lista.Add(produto);
+            lista.Add(novoProduto);
         }
+
+        // Metodo para Verificar Id
+        public bool VerificaId(List<Produto> listaDeProdutos, int id)
+        {
+            return !listaDeProdutos.Any(p => p.Id == id);
+        }
+
+        // Metodo para Buscar produto
+        public Produto BuscaProduto(int buscaId)
+        {
+            Produto produtoEncontrado = lista.FirstOrDefault(p => p.Id == buscaId);
+
+            if (produtoEncontrado != null)
+            {
+                return produtoEncontrado;
+            }
+            else
+            {
+                Console.WriteLine("Nao encontrei esse ID");
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Metodo para Atualizar produto
+
+
+
+
+
+
+
+
+
+
+
+
+        // Metodo para Excluir produto
+
     }
 }
